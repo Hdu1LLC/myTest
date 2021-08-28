57879a4980e7ce4825dc4e84cbaa0a18a3002eaa
Hdu1LLC <1114735121@qq.com>
2021-08-28 16:15:34 +0800

    last

--- test2.cpp
+++ test2.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout << "加油吧少年,再来!!" << endl;
+    cout << "加油吧少年,再来" << endl;
     cout << "dbsuvb" << endl;
     getchar();
     return 0;
